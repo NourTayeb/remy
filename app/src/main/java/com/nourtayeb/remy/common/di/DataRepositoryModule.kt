f9022f47f4a50754a651bcc924ccb77badf1dd68
NourTayeb <nour.tayeb251@gmail.com>
2021-01-31 00:53:56 +0300

    Setup Dagger hilt basic modules + HiltApp (#3)

---
+++ app/src/main/java/com/nourtayeb/remy/common/di/DataRepositoryModule.kt
@@ -0,0 +1,15 @@
+package com.nourtayeb.remy.common.di
+
+import android.content.SharedPreferences
+import com.apollographql.apollo.ApolloClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object DataRepositoryModule {
+
+
+}
