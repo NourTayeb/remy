2c42d114fc522dd20a5fef897143d352be68740c
nourt <apps@jathwa.com>
2021-02-01 00:35:29 +0300

    Created master (movielist) and details (moviedetails) flow with nav component

---
+++ app/src/main/java/com/nourtayeb/remy/presentation/ui/moviedetails/MovieDetailsFragment.kt
@@ -0,0 +1,8 @@
+package com.nourtayeb.remy.presentation.ui.moviedetails
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MovieDetailsFragment:Fragment() {
+}
