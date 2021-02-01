e16a6006500ab8f81953a4be33c98b22536220c5
nourt <apps@jathwa.com>
2021-02-02 00:43:57 +0300

    Show loading and list states for movies list

--- app/src/main/java/com/nourtayeb/remy/presentation/ui/movielist/MovieListFragment.kt
+++ app/src/main/java/com/nourtayeb/remy/presentation/ui/movielist/MovieListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -104,20 +105,20 @@ class MovieListFragment:Fragment() {
     }
 
     fun showToast(string: String) {
-
+        Toast.makeText(requireContext(),string,Toast.LENGTH_LONG).show()
     }
 
     fun hideNoData(){
-
+        binding.noData.visibility = View.GONE
     }
     fun showLoading(){
-
+        binding.loading.visibility = View.VISIBLE
     }
     fun showNoData(){
-
+        binding.noData.visibility = View.VISIBLE
     }
     fun hideLoading(){
-
+        binding.loading.visibility = View.GONE
     }
 
 
