0ea1cb8cf07b77b7abc2953f439595c127c11bc4
nourt <apps@jathwa.com>
2021-02-06 22:44:19 +0300

    Handled server errors while doing infinite loading

--- app/src/main/java/com/nourtayeb/remy/data/mapper/MovieMapper.kt
+++ app/src/main/java/com/nourtayeb/remy/data/mapper/MovieMapper.kt
@@ -7,36 +7,34 @@ import com.nourtayeb.remy.domain.entity.MovieList
 import javax.inject.Inject
 
 class MovieMapper @Inject constructor() {
-    fun remoteToEntity( remoteMovies: PopularQuery.Movies?):MovieList?{
-        try {
-            val movies = remoteMovies!!.popular.edges?.map {
-                Movie(
-                    it!!.cursor,
-                    it.node!!.id,
-                    it.node.title,
-                    it.node.rating,
-                    it.node.poster.toString()
-                )
-            }
-            return MovieList(movies!!, remoteMovies.popular.pageInfo.hasNextPage)
-        }catch (e:Exception){
-            return null
+    fun remoteToEntity(remoteMovies: PopularQuery.Movies?): MovieList? {
+        val movies = remoteMovies!!.popular.edges?.map {
+            Movie(
+                it!!.cursor,
+                it.node!!.id,
+                it.node.title,
+                it.node.rating,
+                it.node.poster.toString()
+            )
         }
-
+        if (movies == null)
+            throw IllegalStateException("Server error!")
+        return MovieList(movies, remoteMovies.popular.pageInfo.hasNextPage)
     }
-    fun remoteToEntity( remoteMovie: DetailsQuery.Movie?):Movie?{
+
+    fun remoteToEntity(remoteMovie: DetailsQuery.Movie?): Movie? {
         try {
 
-                return Movie(
-                    "",
-                    remoteMovie!!.id,
-                    remoteMovie.title,
-                    remoteMovie.rating,
-                    remoteMovie.poster.toString(),
-                    remoteMovie.releaseDate.toString().split("T")[0],
-                    remoteMovie.details.overview
-                )
-        }catch (e:Exception){
+            return Movie(
+                "",
+                remoteMovie!!.id,
+                remoteMovie.title,
+                remoteMovie.rating,
+                remoteMovie.poster.toString(),
+                remoteMovie.releaseDate.toString().split("T")[0],
+                remoteMovie.details.overview
+            )
+        } catch (e: Exception) {
             return null
         }
 
